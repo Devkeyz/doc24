0cd8c0f001fec7acc2e0345775c1198f8e709021
devkeyz <deflyo@gmail.com>
2020-09-28 22:02:05 +0300

    Second Commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	$('.doctors__slider').slick({
+		arrows: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		outline: false,
+		responsive: [
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+		]
+  });
 
 });
